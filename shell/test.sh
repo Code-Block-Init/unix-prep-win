578a1f6d8d514301f7a2bef1359cb2f78ea65301
Aswini S <ashumeow@live.com>
2015-07-03 05:25:17 +0530

    add test script

---
+++ shell/test.sh
@@ -0,0 +1,10 @@
+# first shell script
+pwd
+ls
+
+###################################
+#	dell@DELL3521 /d/codes/shell
+#	$ ./test.sh
+#	/d/codes/shell
+#	test.sh
+###################################
