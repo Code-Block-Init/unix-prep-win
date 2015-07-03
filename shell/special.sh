fddbcdb364bb1f31070625964aa44d42f9c6ef18
Aswini S <ashumeow@live.com>
2015-07-03 09:52:37 +0530

    add command line arguments

---
+++ shell/special.sh
@@ -0,0 +1,8 @@
+echo "File Name: $0" 
+echo "First Name : $1" 
+echo "Last Name : $2" 
+echo "Quoted Values: $@" 
+echo "Quoted Values: $*" 
+echo "Total Number of words : $#"
+
+# ./special.sh Aswini S
