61299d1311930ee84481cb7518f6c039552eb6b6
Aswini S <ashumeow@live.com>
2015-07-03 23:54:04 +0530

    add case

---
+++ shell/case.sh
@@ -0,0 +1,8 @@
+ig="cheese"
+case "$ig" 
+	in "cheese") echo "Woo! J'aime beaucoup de fromages <3"
+	;;
+	"banana") echo "I love banana"
+	;;    
+	"ice-cream") echo "Yummy Strawberry ice-cream"
+	;; esac 
