b12260ac098814a4779fad43bfbbf3fd93affdd8
Aswini S <ashumeow@live.com>
2015-07-06 01:46:53 +0530

    add Vim power

---
+++ shell/doc_file.sh
@@ -0,0 +1,11 @@
+#############################################################
+#	vi $filename <<EndOfCommands 
+#	i 
+#	This file was created automatically from a shell script 
+#	^[ 
+#	ZZ 
+#	EndOfCommands
+#############################################################
+filename=test.txt 
+cat << test.txt
+This is a test file.
