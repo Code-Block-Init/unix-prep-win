aad36678b62ce5198dcad9ab25823e996e9cdc3f
Aswini S <ashumeow@live.com>
2015-07-06 03:59:52 +0530

    add parameters & values to JS mixed with shell

--- shell/function.sh
+++ shell/function.sh
@@ -5,3 +5,25 @@ ig() {
 
 # Invoking a function 
 ig
+
+## returning values
+ig_ig() {
+	## this will return as 199, instead of 1991
+	# return is limited to 3 digits
+	return 1991 
+}
+
+ig_ig
+
+# returns the value of previous command
+birthyear=$? 
+
+## included \1 to display $birthyear as 1991
+echo "Meow was born in "$birthyear\1
+
+## passing parameters
+ig_meow() {
+	echo "Meow loves $1!"
+}
+
+ig_meow cheese
