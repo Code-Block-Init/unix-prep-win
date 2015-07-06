7859f615e9d4a0e6ed8e8f70d676649e8a9aadd7
Aswini S <ashumeow@live.com>
2015-07-06 11:46:22 +0530

    add print integer from float

---
+++ shell/integer.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use 5.022;
+
+# prints integer value from the float value
+
+my $a=int 3.14;
+print $a;
+
+print "\n";
+
+my $b=int 3;
+print $b;
+
+print "\n";
+
+my $c=int 3.99999;
+print $c;
+
+print "\n";
+
+print int -3.14;
