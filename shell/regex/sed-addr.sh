bd2d017c8ee2dedb5c9c745af765dc500ddf0a8d
Aswini S <ashumeow@live.com>
2015-07-06 22:47:59 +0530

    add sed address
    some of them aren't working well though

---
+++ shell/regex/sed-addr.sh
@@ -0,0 +1,9 @@
+# sed addresses
+# cat >> /etc/passwd | sed '1d' | more
+
+# sed address range
+# cat >> /etc/passwd | sed '1, 5d' |more
+
+# cat >> /etc/passwd | sed -n '1,3p'
+
+cat >> /etc/passwd | sed -n
