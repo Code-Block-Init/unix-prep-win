5c0a86960dc56f483d2485ecf337ea5c21135554
Aswini S <ashumeow@live.com>
2015-07-06 23:29:56 +0530

    Update sed.sh

--- shell/regex/sed.sh
+++ shell/regex/sed.sh
@@ -1,1 +1,9 @@
 cat >> /etc/passwd | sed
+
+# cat >> /etc/passwd | sed 's/root/amrood/'
+
+# global substitution
+# cat >> /etc/passwd | sed 's/root/amrood/g'
+
+# using regular expressions
+# cat >> testing | sed '/^daemon/d' 
