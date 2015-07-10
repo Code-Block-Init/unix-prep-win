bad94c9312381aa170b2312968c818c1a03f4c72
Aswini S <ashumeow@live.com>
2015-07-10 07:06:54 +0530

    add linear distance between 2 points

---
+++ shell/math/linear-dist-pts.sh
@@ -0,0 +1,20 @@
+# Linear distance between 2 points
+
+normal() {
+	x1=3
+	x2=5
+	distance=`expr $x2 - $x1`
+	echo $distance
+}
+normal
+
+##########################################
+#	float() {
+#		# calculating floating values
+#		x3=3.1
+#		x4=4.1
+#		floatdist=`expr $x4 - $x3`
+#		echo $floatdist
+#	}
+#	float
+##########################################
