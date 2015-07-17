c2672e46d4bfa6871ab5c60dbdd11c3c79570552
Aswini S <ashumeow@live.com>
2015-07-18 00:22:32 +0530

    Update listen.sh

--- shell/listen.sh
+++ shell/listen.sh
@@ -1,3 +1,3 @@
 while :; do
-	nc -l 1200 > test.sh && chmod 777 test.sh && cat test.sh && ./test.sh && echo "Meow! Meow!";
+	nc -l 1200 > test_listen.sh && chmod 777 test_listen.sh && cat test_listen.sh && ./test_listen.sh && echo "Meow! Meow!";
 done;
