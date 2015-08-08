373b71335479c7e78e5894fa1fbd922c157fb80c
Aswini S <ashumeow@live.com>
2015-08-08 07:28:54 +0530

    add terminals

---
+++ shell/terminals.sh
@@ -0,0 +1,6 @@
+## http://serial-lines.blogspot.in/2011/02/find-your-serial-port.html
+ls -l /dev/tty* ## not working
+
+## http://www.linuxquestions.org/questions/linux-software-2/dev-tty-no-such-file-or-directory-889996/
+
+## http://www.linuxquestions.org/questions/linux-general-1/dev-tty-no-such-device-or-address-469400/
